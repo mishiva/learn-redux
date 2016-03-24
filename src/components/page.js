9610949805040b02b7f3a924250307e8cdb282a7
Mikhail Demidov <mikhail.demidov@saltpepper.ru>
2016-03-24 16:08:00 +0300

    Add simple actions

--- src/components/page.js
+++ src/components/page.js
@@ -1,15 +1,25 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class Page extends Component {
+  onYearBtnClick(e) {
+    this.props.setYear(+e.target.textContent);
+  }
+
   render() {
     const {photos, year} = this.props
     return (
-      <p>Photos have {photos.length} from {year} year</p>
+      <div>
+        <p>You have {photos.length} photos from {year} year</p>
+        <button onClick={::this.onYearBtnClick}>2016</button>
+        <button onClick={::this.onYearBtnClick}>2017</button>
+        <button onClick={::this.onYearBtnClick}>2018</button>
+      </div>
     );
   }
 }
 
 Page.propTypes = {
   year: PropTypes.number.isRequired,
-  photos: PropTypes.array.isRequired
+  photos: PropTypes.array.isRequired,
+  setYear: PropTypes.func.isRequired
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import User from '../components/user';
 import Page from '../components/page';
+import * as pageActions from '../actions/pageActions';
 
 class App extends Component {
   render() {
     const { user, page } = this.props
-    console.log(user);
+    const { setYear } = this.props.pageActions
     return (
       <div>
       <User user={user} />
-      <Page photos={page.photos} year={page.year} />
+      <Page photos={page.photos} year={page.year} setYear={setYear} />
       </div>
     );
   }
@@ -23,4 +25,11 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps)(App);
+function mapDispatchToProps(dispatch) {
+  return {
+    pageActions: bindActionCreators(pageActions, dispatch)
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
